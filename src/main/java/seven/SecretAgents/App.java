56e8b26678b8bdbcefde10411c0547d7b3bd3f6a
costinb7 <costinb7@gmail.com>
2015-02-04 18:54:05 +0200

    initial commit: the project generated by eclipse

---
+++ src/main/java/seven/SecretAgents/App.java
@@ -0,0 +1,13 @@
+package seven.SecretAgents;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
